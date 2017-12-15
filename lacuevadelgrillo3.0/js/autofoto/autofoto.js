d69f5fb97f31baa05311bcf3726fa09949107d6e
Pascual <pascual.munoz@pccomponentes.com>
2017-12-15 21:35:04 +0100

    autofoto rulando!

--- lacuevadelgrillo3.0/js/autofoto/autofoto.js
+++ lacuevadelgrillo3.0/js/autofoto/autofoto.js
@@ -1,11 +1,12 @@
 //https://stackoverflow.com/questions/18480550/how-to-load-all-the-images-from-one-of-my-folder-into-my-web-page-using-jquery
+    console.log('ejecutando ajax');
     $.ajax({
-        url: "getImages.php",
+        url: "./js/autofoto/getImages.php",
         dataType: "json",
         success: function (data) {
 
             $.each(data, function(i,filename) {
-                $('#imageDiv').prepend('<img src="'+ filename +'"><br>');
+                $('#autofoto').prepend('<img id="' + filename + '" src="'+ filename +'"><br>');
             });
         }
     });
