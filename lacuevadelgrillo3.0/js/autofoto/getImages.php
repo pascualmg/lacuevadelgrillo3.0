189d5b9839e517c942651f45df69edb01a3560a2
Pascual <pascual.munoz@pccomponentes.com>
2017-12-15 21:33:16 +0100

    autofoto rulando! foto de test de la pinada

--- lacuevadelgrillo3.0/js/autofoto/getImages.php
+++ lacuevadelgrillo3.0/js/autofoto/getImages.php
@@ -5,13 +5,29 @@
  * Date: 15/12/2017
  * Time: 19:15
  */
-$filenameArray = [];
+$relativeDir = '/fotos/';
+function cropFrom($from, $str)
+{
+    $index = strrpos(
+        $str,
+        $from
+    );
 
-$handle = opendir(dirname(realpath(__FILE__)).'../images/');
-while($file = readdir($handle)){
-    if($file !== '.' && $file !== '..'){
-        array_push($filenameArray, "images/$file");
-    }
+    return substr(
+        $str,
+        $index,
+        strlen($str) - $index
+        );
 }
 
+$filenameArray = [];
+$putoPath = dirname(realpath(__FILE__)).$relativeDir;
+$handle = opendir($putoPath);
+while($file = readdir($handle)){
+    if($file !== '.' && $file !== '..'){
+        $absolutePathWithFilename = "{$putoPath}/{$file}";
+
+        $filenameArray[] = cropFrom('js',$absolutePathWithFilename);
+    }
+}
 echo json_encode($filenameArray);
