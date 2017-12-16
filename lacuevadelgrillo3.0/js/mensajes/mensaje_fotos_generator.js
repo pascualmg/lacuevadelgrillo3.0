92b94885ed3c5dcccaa52d82c2b8b220284d4f73
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2017-12-16 02:55:40 +0100

    mensaje autoactualizado segun fin y comiendo de ano

---
+++ lacuevadelgrillo3.0/js/mensajes/mensaje_fotos_generator.js
@@ -0,0 +1,15 @@
+var dateObj = new Date();
+var month = dateObj.getUTCMonth() + 1; //months from 1-12
+var day = dateObj.getUTCDate();
+var year = dateObj.getUTCFullYear();
+
+var terminandoAnio = (2017 == year)?"últimas":"primeras";
+
+$('#mensaje_fotos').prepend(
+    '<h2>Aquí están las ' +
+    terminandoAnio +
+    ' fotos de ' +
+    year +
+    ' tras las últimas reformas , esperamos que os gusten!' +
+    '</h2>'
+);
