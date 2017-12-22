81fc663af0adbf37e72bd73752903eee0f1ceb70
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2017-12-22 09:07:51 +0100

    subscriber

--- lacuevadelgrillo3.0/js/rx/operator.js
+++ lacuevadelgrillo3.0/js/rx/operator.js
@@ -12,11 +12,15 @@ let bar$;
 // filter(predicate) -> predictate => v % 2 ;
 
 bar$ = foo$
-.filter(v => v%2)
+.filter(v => v%2);
 // {...1...3....5...7..}
 
 
 //take , devuelve los X primeros numeros de la secuencia.
+bar$ = foo$.first(a => console.log(a));
 
-
-
+const subs = bar$.subscribe(
+ value = console.log(`next{value}`),
+    err = console.log(`err{value}`),
+    end = console.log(`end{end}`)
+);
