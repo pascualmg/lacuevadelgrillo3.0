a288cd643942d35d7775e6e3dfc08886bf9c24ba
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2017-12-22 09:46:31 +0100

    algunos observables.

--- lacuevadelgrillo3.0/js/rx/operator.js
+++ lacuevadelgrillo3.0/js/rx/operator.js
@@ -1,3 +1,4 @@
+//Hay tipos de observables finitos e infinitos.
 
 
 
@@ -23,16 +24,44 @@ bar$ = foo$
 
 //Operador disctinct().
 
+console.log('disctinctUntilChanged');
 bar$ = Rx.Observable
     .of('a','b','c','d','e')
-    .distinctUntilChanged();
+    .distinctUntilChanged()
+    .do(a => console.log(a))
+;
+
+//combinaciones concat.
+console.clear();
+// {...0..1..2..3...4}
+let foo2$ = Rx.Observable
+    .interval(500)
+    .take(4);
+
+let baz$ = Rx.Observable
+    .of(4,5,6,7,8,9)
+baz$ = Rx.Observable.concat(foo$,baz$);
 
 
+//startwith{""cheduler, ...args_)
+baz$ = foo$.startWith('hi', 'all')
+    .do(a => console.log(a));
 
-const subs = bar$.subscribe(
- value = console.log(`next{value}`),
-    err = console.log(`err{value}`),
-    end = console.log(`end{end}`)
+//zip . (...arg, {scheduler}
+// Une secuencias como una cremallera , tan pronoto como tiene parejas. devuelve un array x, y o lo que devuelve a resultSelector (value,
+//     recube un parametro ,  Termina tan pronto como uno de los observables o secuencias finaliza y hace una pareja )
+// {0...1...2...3...4..}
+// {4,4,5,5,6,7,6}
+
+baz$ = Rx.Observable.zip(foo$, bar$)
+    .do(a => console.log(a))
+;
+
+
+const observerA = bar$.subscribe(
+ value => console.log(`next ${value}`),
+    err => console.log(`err ${err}`),
+    end => console.log(`fin`)
 );
 
 
