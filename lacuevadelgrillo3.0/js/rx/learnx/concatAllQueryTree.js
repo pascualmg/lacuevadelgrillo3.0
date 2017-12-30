bea320a2dc7b92da27a9884a1561c8ae28dc7fc6
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2017-12-30 19:48:46 +0100

    ejercicio 9 y 10 learnrx done.

--- lacuevadelgrillo3.0/js/rx/learnx/concatAllQueryTree.js
+++ lacuevadelgrillo3.0/js/rx/learnx/concatAllQueryTree.js
@@ -1,3 +1,100 @@
+/* Ejercicio 10 ,about concatAll(),  si tenemos un tree ( treeFoo que no es más que un array de más de 2 dimensiones ...
+   implementamos una concatAll , que se usa para obtener un array unidimensional sobre el que
+   sí que podamos aplicar las funciones como map, filter y analogas.
+
+    Vamos a  sobrescribir los metodos con con el prototype de la clase array concatAll y filter
+ */
+console.clear();
+
+var treeFoo = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+/**
+ * Override de la built-in, dado un arrFoo
+ * @returns {Array}
+ */
+Array.prototype.miConcatAll = function () {
+    var results = [];
+    this.forEach(
+        function (subArray) {
+            subArray.forEach(
+                function (item) {
+                    results.push(item);
+                }
+            );
+        }
+    );
+    return results;
+};
+
+Array.prototype.miFilter = function (predicateFunction) {
+    var results = [];
+
+    this.forEach(
+        function (item) {
+            if (predicateFunction(item)) {
+                results.push(item);
+            }
+        }
+    );
+    return results;
+};
+
+Array.prototype.miMap = function(projectionFunction) {
+   var results = [];
+
+   this.forEach
+   (
+       function(item){
+           results.push(projectionFunction(item));
+       }
+   );
+   return results;
+};
+
+/**
+ * predicate Function, para fistros
+ * @param item
+ * @returns {boolean}
+ */
+function losMenoresDeCinco(item)
+{
+   return item < 5;
+};
+
+/**
+ * projection Function, para los mapeos.
+ * @param item
+ * @returns {string}
+ */
+function toHumanWords(item)
+{
+    var numberName = [
+        'cero',
+        'uno',
+        'dos',
+        'tres',
+        'cuatro',
+        'cinco',
+        'seis',
+        'siete',
+        'ocho',
+        'nueve'
+    ];
+    return numberName[item];
+};
+
+console.log('Ejercicio 10 learnRx')
+console.log('si tenemos el tree ', treeFoo);
+console.log('cuando aplicamos treefoo.concatAll() obtenemos un array con los ids', treeFoo.miConcatAll());
+console.log('esto lo podemos filtrar', treeFoo.miConcatAll().miFilter(losMenoresDeCinco));
+console.log('y luego lo podemos mapear o lo que haga falta...',
+    treeFoo
+        .miConcatAll()
+        .miFilter(losMenoresDeCinco)
+        .miMap(toHumanWords)
+);
+
+//funcion del ejercicio 9, con un foreach
 function concatAllQyeryTree() {
     var movieLists = [
             {
@@ -53,8 +150,8 @@ function concatAllQyeryTree() {
     movieLists.forEach(
         function (movieListsItem) {
             movieListsItem.videos.forEach(
-                function(video){
-                   allVideoIdsInMovieLists.push(video.id);
+                function (video) {
+                    allVideoIdsInMovieLists.push(video.id);
                 }
             )
         }
